3ef5dc1ac3d2ae5f3151a957e0a21829547de5bb
Dawid Czyzewski <vinciebr@proton.me>
2023-05-27 10:20:32 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -4,44 +4,65 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 function listContacts() {
-  const contacts = fs.readFileSync(contactsPath, "utf-8");
-  const contactsParsed = JSON.parse(contacts);
-  // console.log(contactsParsed);
+  try {
+    const contactsData = fs.readFileSync(contactsPath, "utf-8");
+    const contactsParsed = JSON.parse(contactsData);
+    // console.log(contactsParsed);
 
-  console.table(contactsParsed);
-  return contactsParsed;
+    console.table(contactsParsed);
+    return contactsParsed;
+  } catch (error) {
+    console.log("something gone wrong: ", error);
+  }
 }
 
 function getContactById(contactId) {
-  const contacts = listContacts();
-  const contact = contacts.find((contact) => contact.id === contactId);
-  return contact;
+  try {
+    const contacts = listContacts();
+    const contact = contacts.filter((contact) => contact.id === contactId);
+    console.log("found contact: ", contact);
+    return contact;
+  } catch (error) {
+    console.log("something gone wrong: ", error);
+  }
 }
 
 function removeContact(contactId) {
-  const contacts = listContacts();
-  const filteredContacts = contacts.filter(
-    (contact) => contact.id !== contactId
-  );
-  // console.log(filteredContacts);
-  const filteredContactsStringified = JSON.stringify(filteredContacts, null, 2);
-  // console.log(filteredContactsStringified);
-  fs.writeFileSync(contactsPath, filteredContactsStringified);
-  return filteredContacts;
+  try {
+    const contacts = listContacts();
+    const filteredContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    // console.log(filteredContacts);
+    const filteredContactsStringified = JSON.stringify(
+      filteredContacts,
+      null,
+      2
+    );
+    // console.log(filteredContactsStringified);
+    fs.writeFileSync(contactsPath, filteredContactsStringified);
+    return filteredContactsStringified;
+  } catch (error) {
+    console.log("something gone wrong: ", error);
+  }
 }
 
 function addContact(name, email, phone) {
-  const contacts = listContacts();
-  const newContact = {
-    id: Date.now().toString(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  const contactsStringified = JSON.stringify(contacts, null, 2);
-  fs.writeFileSync(contactsPath, contactsStringified);
-  return newContact;
+  try {
+    const contacts = listContacts();
+    const newContact = {
+      id: Date.now().toString(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    const contactsStringified = JSON.stringify(contacts, null, 2);
+    fs.writeFileSync(contactsPath, contactsStringified);
+    return newContact;
+  } catch (error) {
+    console.log("something gone wrong: ", error);
+  }
 }
 
 module.exports = {
